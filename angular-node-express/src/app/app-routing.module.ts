e9892f4834c355f2f4f3690e3a41289f14685894
Sarthak Jain <jainsarthak216@gmail.com>
2022-06-28 14:14:14 +0530

    Added assignment

---
+++ angular-node-express/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { InputUserDataFormComponent } from './input-user-data-form/input-user-data-form.component';
+import { DisplayUserDataComponent } from './display-user-data/display-user-data.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: InputUserDataFormComponent
+  },
+  {
+    path: 'user/:uid',
+    component: DisplayUserDataComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ angular-node-express/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { InputUserDataFormComponent } from './input-user-data-form/input-user-data-form.component';
+import { DisplayUserDataComponent } from './display-user-data/display-user-data.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InputUserDataFormComponent,
+    DisplayUserDataComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
